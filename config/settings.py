cd4e630828142f1d8f42f6565b6e931a09e79c5f
sorinandreienache <sorin.enache@edu.unifi.it>
2026-07-12 23:11:30 +0200

    Abilita login via browser (SessionAuthentication)

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ AUTH_USER_MODEL = 'accounts.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
