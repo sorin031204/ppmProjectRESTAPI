5a2716a5677da5931c480f476d6e04fcc192f081
sorinandreienache <sorin.enache@edu.unifi.it>
2026-07-12 23:15:14 +0200

    Aggiunge URL login

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/login/', obtain_auth_token, name='api-login'),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('shop.urls')),
 ]
